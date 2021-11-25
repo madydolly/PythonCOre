3784cec1e9ab05ab39ad31fb610377642cc9eca0
madydolly <58760412+madydolly@users.noreply.github.com>
2021-11-25 17:19:39 +0200

    First time

---
+++ print('hello').py
@@ -0,0 +1,8 @@
+print('hello')
+name = input('What is your name?')
+age = int(input('How old are you?'))
+city = input('Where do you live?')
+print('HEllo ' + name)
+print('Your age is', age)
+print ('You live in '+ city)
+
